27ddaa29302dcea0c6b5f2f86e68aa6e7edf78e4
Troyy <99565211+Troyy1@users.noreply.github.com>
2023-03-18 22:46:10 +0000

    Create Surface.cs

---
+++ Surface.cs
@@ -0,0 +1,13 @@
+namespace GorillaMove
+{
+    using UnityEngine;
+
+    public class Surface : MonoBehaviour
+    {
+        public const float DefaultSlipPercentage = 0.001f;
+        public const float DefaultSlipPercentageHug = 0.03f;
+
+        public float slipPercentage;
+
+    }
+}
